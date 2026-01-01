e1f52264a7a2c29644374c844383170c0695c6d3
Aya Mousa <alrawadiahayah@gmail.com>
2026-01-01 17:37:20 +0300

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Browser-extension-manager-UI/',
+  base: '/extensions-manager/',
 })
+
